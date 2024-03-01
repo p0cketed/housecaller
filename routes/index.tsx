270927a5ef235e83b77ef852874024905e242f3b
p0cketed <capogreen14@gmail.com>
2024-03-01 15:11:00 -0800

    wyatt face

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,22 @@
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+
 
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-6 mx-auto bg-blue-200">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
-          src="/logo.svg"
+          src="/wyattface.jpg"
           width="128"
           height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+          alt="Wyatt"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Housecaller</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          What days are client available?
         </p>
-        <Counter count={count} />
       </div>
     </div>
   );
